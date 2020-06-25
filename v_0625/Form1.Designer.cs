ee58795a23e3780e3a6535f3b6a3fd69caffbd24
fusatoshi3104 <satoshidat@gmail.com>
2020-06-25 11:59:00 +0900

    ボタンを消す

--- v_0625/Form1.Designer.cs
+++ v_0625/Form1.Designer.cs
@@ -33,12 +33,13 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(284, 158);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
--- v_0625/Form1.cs
+++ v_0625/Form1.cs
@@ -19,8 +19,9 @@ namespace v_0625
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int a = 0;
+            int a = 1;
             MessageBox.Show(""+(10/a));
+            button1.Visible = false;
         }
     }
 }
